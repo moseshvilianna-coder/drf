2967c202fca168f998309b80b9836e5cbfd60b5d
moseshvilianna-coder <moseshvilianna@gmail.com>
2026-03-08 20:01:24 +0400

    add permissions

--- snippets/views.py
+++ snippets/views.py
@@ -9,6 +9,7 @@ from .serializers import SnippetSerializer, UserSerializer
 class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
@@ -17,6 +18,7 @@ class SnippetList(generics.ListCreateAPIView):
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
 
 
 class UserList(generics.ListAPIView):
