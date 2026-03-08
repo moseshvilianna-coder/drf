4cf3527e7312c51462ceb07635c0250b24c9f214
moseshvilianna-coder <moseshvilianna@gmail.com>
2026-03-08 22:02:22 +0400

    highlighted snippet endpoint

--- snippets/views.py
+++ snippets/views.py
@@ -1,6 +1,6 @@
 # snippets/views.py
 from django.contrib.auth.models import User
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, renderers
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
@@ -8,6 +8,15 @@ from rest_framework.reverse import reverse
 from .models import Snippet
 from .permissions import IsOwnerOrReadOnly
 from .serializers import SnippetSerializer, UserSerializer
+
+
+class SnippetHighlight(generics.GenericAPIView):
+    queryset = Snippet.objects.all()
+    renderer_classes = (renderers.StaticHTMLRenderer,)
+
+    def get(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
 
 
 @api_view(["GET"])
